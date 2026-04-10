5444400c99c708240123d529b19962f39594c894
doykovivan4-cloud <doykov.ivan4@gmail.com>
2026-04-10 09:16:16 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
-
 using namespace std;
+
 int main() {
-    int number; // Объявляем переменную для хранения числа
-    cin >> number; // Считываем число с клавиатуры
+    setlocale(LC_ALL, "Russian");
 
-    // Ваш код:
-if(number==10)
+    int n;
+    cin >> n;
+switch(n)
 {
-cout << "Число равно 10";
-}
-else
-{
-    cout <<"Число не равно 10";
+case 1: cout <<"Понедельник";
+    break;
+case 2: cout <<"Вторник";
+    break;
+case 3: cout <<"Среда";
+    break;
+case 4: cout <<"Четверг";
+    break;
+case 5: cout <<"Пятница";
+    break;
+case 6: cout <<"Суббота";
+    break;
+case 7: cout <<"Воскресенье";
+    break;
+    default: cout <<"Неверный день";
 }
 
 
-    return 0; // Завершаем программу
+
+    return 0;
 }
